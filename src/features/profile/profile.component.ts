c3c7cc1be75c84e78ddcc133cf1fc2b9c2cc3e07
thaiane-pereira <thaaianevs2010@gmail.com>
2024-09-27 19:26:33 -0300

    feat: Implementing property binding on html

--- src/features/profile/profile.component.ts
+++ src/features/profile/profile.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  userData = {name: "Lucas Pereira", nickname:"Lucas", user:"lpmelo", email:"lpmelo10@gmail.com"}
 
 }
