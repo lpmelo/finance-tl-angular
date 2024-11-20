fafb7c73410bf89a28aa68615f309e4eee4083f5
lpmelo <lpmelo10@gmail.com>
2024-11-19 23:03:07 -0300

    feat: Implementing form

--- src/features/transaction/footer/footer.component.ts
+++ src/features/transaction/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectLayoutIsMobileDevice } from '../../../store/layout-reducer/layout.selectors';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'transaction-footer',
@@ -9,5 +10,10 @@ import { selectLayoutIsMobileDevice } from '../../../store/layout-reducer/layout
 })
 export class FooterComponent {
   store = inject(Store);
+  location = inject(Location);
   $isMobile = this.store.selectSignal(selectLayoutIsMobileDevice);
+
+  handleClickCancel() {
+    this.location.back();
+  }
 }
--- src/features/transaction/transaction.component.ts
+++ src/features/transaction/transaction.component.ts
@@ -1,5 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
+const transactionFormInitialState = new FormGroup({
+  type: new FormControl('', [Validators.required]),
+  gender: new FormControl('', [Validators.required]),
+  description: new FormControl('', [Validators.required]),
+  value: new FormControl('', [Validators.required]),
+  date: new FormControl('', [Validators.required]),
+  plotNumber: new FormControl('', []),
+});
 
 @Component({
   selector: 'app-transaction',
@@ -9,8 +19,16 @@ import { Router } from '@angular/router';
 export class TransactionComponent implements OnInit {
   router = inject(Router);
   editMode = false;
+  transactionForm: FormGroup = transactionFormInitialState;
 
   ngOnInit(): void {
     this.editMode = this.router.url.includes('/edit');
+  }
+
+  returnTitle() {
+    if (this.editMode) {
+      return 'Editar transação';
+    }
+    return 'Nova transação';
   }
 }
