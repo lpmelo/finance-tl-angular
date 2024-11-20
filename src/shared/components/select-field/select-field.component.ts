2e21075eda9580136fe8d1860cdc5c2d9b71258f
lpmelo <lpmelo10@gmail.com>
2024-11-19 23:19:55 -0300

    fix: Fixing select component

--- src/shared/components/select-field/select-field.component.ts
+++ src/shared/components/select-field/select-field.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-select-field',
+  selector: 'select-field',
   templateUrl: './select-field.component.html',
   styleUrl: './select-field.component.scss',
 })
